03faaaacc40316d1a2487f39cd5d3cb673237b5c
tarekelkanaria <tarekelkanaria@gmail.com>
2025-11-17 06:13:53 -0500

    Update cart item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,47 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+        const {cost, quantity} = item
+        total += parseFloat(cost.substring(1)) * quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e)
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+    if(item.quantity > 1){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }else{
+        dispatch(removeItem(item.name))
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const {cost, quantity} = item;
+    const totalCost = parseFloat(cost.substring(1)) * quantity;
+    return totalCost;
   };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+};
 
   return (
     <div className="cart-container">
